7f26108a0ffa4aa833cdd55043b70bf7289f0c4f
ailuroq <yaroslav_ovsyannikov@list.ru>
2020-12-22 19:17:18 +0300

    Initial commit

---
+++ src/main/java/com/buzz/BuzzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzzBackendApplication.class, args);
+	}
+
+}
